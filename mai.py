d14be68b3fd6ec2dcb9f05a186724784248d6a4b
SArcD <141703135+SArcD@users.noreply.github.com>
2024-08-25 23:10:15 -0600

    Update mai.py

--- mai.py
+++ mai.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from fpdf import FPDF
-import base64
 
 # Función para mostrar información del autor
 def mostrar_acerca_del_autor(foto, nombre, grado, reseña, correo, perfil_scholar):
@@ -9,7 +8,7 @@ def mostrar_acerca_del_autor(foto, nombre, grado, reseña, correo, perfil_schola
     st.write(f"**Grado:** {grado}")
     st.write(reseña)
     st.write(f"**Correo:** {correo}")
-    st.write(f"**Perfil:** {perfil_scholar}")
+    st.write(f"**Perfil:** [Google Scholar]({perfil_scholar})")
 
 
 # Función para mostrar información sobre la plática
@@ -26,22 +25,20 @@ def mostrar_diapositivas(enlace_pdf):
         st.write("No hay diapositivas disponibles para esta plática.")
 
 dates = {
-    "30 de agosto": "30 de agosto: Miguel Huerta",
-    "06 de septiembre": "06 de septiembre: Mónica Ríos",
-    "13 de septiembre": "13 de septiembre: Mariano, Julio y Minerva",
-    "20 de septiembre": "20 de septiembre: Daniela Rios y Victoria García y Julián",
-    "27 de septiembre": "27 de septiembre: Julián",
-    "04 de octubre": "04 de octubre: Fernanda y Cesar",
-    "11 de octubre": "11 de octubre: Yoli y Mónica Ríos",
-    "18 de octubre": "18 de octubre: Ricardo y Fernanda García",
-    "25 de octubre": "25 de octubre: Liliana y Alondra",
-    "08 de noviembre": "08 de noviembre: Paola García y Angie Del Toro",
-    "15 de noviembre": "15 de noviembre: Xóchitl Trujillo",
-    "22 de noviembre": "22 de noviembre: Santiago Arceo",
-    "29 de noviembre": "29 de noviembre: Alberto Bricio y Ricardo Marentes y Valeria Ibarra"
+    "30 de agosto: Miguel Huerta": "30 de agosto: Miguel Huerta",
+    "06 de septiembre: Mónica Ríos": "06 de septiembre: Mónica Ríos",
+    "13 de septiembre: Mariano, Julio y Minerva": "13 de septiembre: Mariano, Julio y Minerva",
+    "20 de septiembre: Daniela Rios y Victoria García y Julián": "20 de septiembre: Daniela Rios y Victoria García y Julián",
+    "27 de septiembre: Julián": "27 de septiembre: Julián",
+    "04 de octubre: Fernanda y Cesar": "04 de octubre: Fernanda y Cesar",
+    "11 de octubre: Yoli y Mónica Ríos": "11 de octubre: Yoli y Mónica Ríos",
+    "18 de octubre: Ricardo y Fernanda García": "18 de octubre: Ricardo y Fernanda García",
+    "25 de octubre: Liliana y Alondra": "25 de octubre: Liliana y Alondra",
+    "08 de noviembre: Paola García y Angie Del Toro": "08 de noviembre: Paola García y Angie Del Toro",
+    "15 de noviembre: Xóchitl Trujillo": "15 de noviembre: Xóchitl Trujillo",
+    "22 de noviembre: Santiago Arceo": "22 de noviembre: Santiago Arceo",
+    "29 de noviembre: Alberto Bricio y Ricardo Marentes y Valeria Ibarra": "29 de noviembre: Alberto Bricio y Ricardo Marentes y Valeria Ibarra"
 }
-
-
 
 # Función para generar un PDF con la información de la página
 def generar_pdf(foto, nombre, grado, reseña, correo, perfil_scholar,  resumen_platica, enlace_pdf):
@@ -65,7 +62,6 @@ def generar_pdf(foto, nombre, grado, reseña, correo, perfil_scholar,  resumen_p
 
     pdf.ln(10)  # Añadir un espacio vertical después de la sección "Acerca del autor"
 
-
     # Sobre la plática
     pdf.set_y(170)  # Ajustar la posición para empezar después de la imagen y texto
     pdf.set_font("Arial", "B", 12)
@@ -75,7 +71,6 @@ def generar_pdf(foto, nombre, grado, reseña, correo, perfil_scholar,  resumen_p
 
     pdf.ln(10)  # Añadir un espacio vertical después de la sección "Sobre la plática"
 
-    
     # Enlace al PDF de diapositivas
     if enlace_pdf:
         pdf.set_font("Arial", "B", 12)
@@ -93,7 +88,12 @@ def pagina_ejemplo():
     foto = "DrHuerta.jpg"  # Cambia esta ruta a la imagen del autor
     nombre = "Miguel Huerta"
     grado = "PhD en Ciencias"
-    reseña = "Miguel Huerta Doctor en Ciencias con especialidad en Fisiología y Biofísica. Es Profesor-Investigador Titular C del Centro Universitario de Investigaciones Biomédicas de la Universidad de Colima. Es miembro del Sistema Nacional de Investigadores en el nivel 3 emérito. Su campo de investigación es la Biomedicina, con énfasis en la fisiología y biofísica del sistema neuromuscular y la fisiopatología de la diabetes mellitus. Ha publicado más de cien artículos revistas indizadas al Journal of Citation Reports y ha graduado a más de 40 Maestros y Doctores en Ciencias en programas SNP-CONAHCyT."
+    reseña = ("Miguel Huerta Doctor en Ciencias con especialidad en Fisiología y Biofísica. "
+              "Es Profesor-Investigador Titular C del Centro Universitario de Investigaciones Biomédicas "
+              "de la Universidad de Colima. Es miembro del Sistema Nacional de Investigadores en el nivel 3 emérito. "
+              "Su campo de investigación es la Biomedicina, con énfasis en la fisiología y biofísica del sistema neuromuscular "
+              "y la fisiopatología de la diabetes mellitus. Ha publicado más de cien artículos revistas indizadas al Journal "
+              "of Citation Reports y ha graduado a más de 40 Maestros y Doctores en Ciencias en programas SNP-CONAHCyT.")
     correo = "miguel.huerta@example.com"
     perfil_scholar = "https://scholar.google.com.mx/citations?user=7jGGpnoAAAAJ&hl=en&oi=ao"
     
@@ -112,7 +112,7 @@ def pagina_ejemplo():
     mostrar_diapositivas(enlace_pdf)
     
     # Botón para generar y descargar el PDF
-    pdf = generar_pdf(foto, nombre, grado, reseña, correo,perfil_scholar, resumen_platica, enlace_pdf)
+    pdf = generar_pdf(foto, nombre, grado, reseña, correo, perfil_scholar, resumen_platica, enlace_pdf)
     st.download_button(
         label="Descargar PDF con los datos del evento",
         data=pdf,
@@ -122,37 +122,19 @@ def pagina_ejemplo():
 
 # Página principal
 def main():
-    if "page" not in st.session_state:
-        st.session_state.page = "Inicio"
-    
     st.sidebar.title("Calendario de Eventos Académicos")
-    page = st.sidebar.selectbox(
+    
+    # Usa un selectbox para seleccionar la página
+    selected_page = st.sidebar.selectbox(
         "Selecciona una fecha para ver más detalles:",
-        [
-            "Inicio",
-            "30 de agosto: Miguel Huerta",
-            "06 de septiembre: Mónica Ríos",
-            "13 de septiembre: Mariano, Julio y Minerva",
-            "20 de septiembre: Daniela Rios y Victoria García y Julián",
-            "27 de septiembre: Julián",
-            "04 de octubre: Fernanda y Cesar",
-            "11 de octubre: Yoli y Mónica Ríos",
-            "18 de octubre: Ricardo y Fernanda García",
-            "25 de octubre: Liliana y Alondra",
-            "08 de noviembre: Paola García y Angie Del Toro",
-            "15 de noviembre: Xóchitl Trujillo",
-            "22 de noviembre: Santiago Arceo",
-            "29 de noviembre: Alberto Bricio y Ricardo Marentes y Valeria Ibarra",
-        ],
-        index=["Inicio", "30 de agosto: Miguel Huerta", "06 de septiembre: Mónica Ríos", "13 de septiembre: Mariano, Julio y Minerva",
-               "20 de septiembre: Daniela Rios y Victoria García y Julián", "27 de septiembre: Julián", "04 de octubre: Fernanda y Cesar",
-               "11 de octubre: Yoli y Mónica Ríos", "18 de octubre: Ricardo y Fernanda García", "25 de octubre: Liliana y Alondra",
-               "08 de noviembre: Paola García y Angie Del Toro", "15 de noviembre: Xóchitl Trujillo", "22 de noviembre: Santiago Arceo",
-               "29 de noviembre: Alberto Bricio y Ricardo Marentes y Valeria Ibarra"].index(st.session_state.page)
+        list(dates.keys())
     )
     
-    if page == "Inicio":
-        st.session_state.page = "Inicio"
+    # Guarda la página seleccionada en session_state
+    st.session_state.page = dates[selected_page]
+    
+    # Lógica para mostrar la página seleccionada
+    if st.session_state.page == "Inicio":
         st.title("Calendario de Eventos Académicos")
         st.write("Haz clic en una fecha para ver más detalles sobre el evento.")
         # Mostrar el calendario de eventos con botones

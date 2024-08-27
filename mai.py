233f629f9536a705f1349d33456e82f925439bb5
SArcD <141703135+SArcD@users.noreply.github.com>
2024-08-27 11:25:09 -0600

    Update mai.py

--- mai.py
+++ mai.py
@@ -94,7 +94,7 @@ def pagina_ejemplo():
     perfil_scholar = "https://scholar.google.com.mx/citations?user=7jGGpnoAAAAJ&hl=en&oi=ao"
     
     # Mostrar sección "Acerca del autor"
-    mostrar_acerca_del_autor(foto, nombre, grado, reseña correo, perfil_scholar)
+    mostrar_acerca_del_autor(foto, nombre, grado, reseña, correo, perfil_scholar)
     
     # Información sobre la plática
     resumen_platica = ("Esta plática abordará la creación y tipos de hipótesis en la investigación científica, "
